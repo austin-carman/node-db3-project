f9129817175a64b0e0bf725f24f2b971d96d485b
Austin Carman <abcarman12@gmail.com>
2021-06-09 16:29:21 -0600

    fleshed out findById in model

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,102 +1,57 @@
 const db = require('../../data/db-config');
 
 function find() {
-  return db.select('sc.*') // what is the short hand similar to db('schemes as sc')... how to include column names
+  return db.select('sc.*')
     .from('schemes as sc')
     .count('st.step_id as number_of_steps')
     .leftJoin('steps as st', 'sc.scheme_id', '=', 'st.scheme_id')
     .groupBy('sc.scheme_id')
     .orderBy('sc.scheme_id');
-  /*
-      SELECT
-          sc.*,
-          count(st.step_id) as number_of_steps
-      FROM schemes as sc
-      LEFT JOIN steps as st
-          ON sc.scheme_id = st.scheme_id
-      GROUP BY sc.scheme_id
-      ORDER BY sc.scheme_id ASC;
-  */
 }
 
 async function findById(scheme_id) {
-  console.log('findById model');
   const scheme = await db.select('sc.scheme_name', 'st.*')
     .from('schemes as sc')
     .leftJoin('steps as st', 'sc.scheme_id', '=', 'st.scheme_id')
     .where('sc.scheme_id', scheme_id)
     .orderBy('st.step_number');
-    
-    let organizedScheme = {
-      scheme_id: '',
-      scheme_name: '',
-      steps: []
-    }
 
-    const test = scheme.map(singleScheme => {
-      return {
-        ...organizedScheme, 
-        scheme_id: singleScheme.scheme_id,
-        scheme_name: singleScheme.scheme_name,
-        steps: [
-          ...organizedScheme.steps, 
-          {
-            step_id: singleScheme.step_id,
-            step_number: singleScheme.step_number,
-            instructions: singleScheme.instructions
-          }
-        ]
-      }
-    })
+    const schemeSteps = scheme.map(singleScheme => {
+        return {
+          step_id: singleScheme.step_id,
+          step_number: singleScheme.step_number,
+          instructions: singleScheme.instructions
+        }
+    });
 
-    return test;
+    const organizedScheme = {
+      scheme_id: scheme[0].scheme_id,
+      scheme_name: scheme[0].scheme_name,
+      steps: schemeSteps[0].step_id == null ? [] : schemeSteps,
+    };
 
-  /*
-      SELECT
-          sc.scheme_name,
-          st.*
-      FROM schemes as sc
-      LEFT JOIN steps as st
-          ON sc.scheme_id = st.scheme_id
-      WHERE sc.scheme_id = 1
-      ORDER BY st.step_number ASC;
-
-    4B- Using the array obtained and vanilla JavaScript, create an object with
-    the structure below, for the case _when steps exist_ for a given `scheme_id`:
-
-      {
-        "scheme_id": 1,
-        "scheme_name": "World Domination",
-        "steps": [
-          {
-            "step_id": 2,
-            "step_number": 1,
-            "instructions": "solve prime number theory"
-          },
-          {
-            "step_id": 1,
-            "step_number": 2,
-            "instructions": "crack cyber security"
-          },
-          // etc
-        ]
-      }
-
-    5B- This is what the result should look like _if there are no steps_ for a `scheme_id`:
-
-      {
-        "scheme_id": 7,
-        "scheme_name": "Have Fun!",
-        "steps": []
-      }
-  */
+    return organizedScheme;
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) {
+  const steps = await db
+    .select('st.step_id', 'st.step_number', 'instructions', 'scheme_name')
+    .from('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', '=', 'st.scheme_id')
+    .groupBy('st.step_number');
+
+
+  return steps;
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
     should be empty if there are no steps for the scheme:
+
+    select st.step_id, st.step_number, instructions, scheme_name
+    from schemes as sc
+    left join steps as st 
+      on sc.scheme_id = st.scheme_id
+    group by st.step_number;
 
       [
         {
